6a415573da7f5dc20b6fe5eea560918cd4ec6e17
gbmahone <oliveiragabrielam@hotmail.com>
2025-11-15 11:03:47 +0000

    Saving code noncommited

--- aulaJPA/src/main/java/Program.java
+++ aulaJPA/src/main/java/Program.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Program {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("aulajpa");
+        EntityManager em = emf.createEntityManager();
+
+        Pessoa p = em.find(Pessoa.class, 2);
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+
+        System.out.println("Pronto!");
+    }
 }
